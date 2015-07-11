4a25b436ba6c44866770881e28f74dcd844db20c
Bohdan Hontarenko <tuoefil@gmail.com>
2015-07-11 22:07:33 +0300

    major gulpfile cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,15 @@
 'use strict';
 
-var g        = require('gulp-load-plugins')();
-var argv     = require('yargs').argv;
-var gulp     = require('gulp');
-var os       = require('os');
-var es       = require('event-stream');
-var parallel = require('concurrent-transform');
-
-var browserify = require('browserify'),
-    watchify = require('watchify'),
-    babelify = require('babelify'),
-    runSequence = require('run-sequence'),
-    source = require('vinyl-source-stream'),
-    buffer = require('vinyl-buffer'),
-    merge = require('merge-stream'),
-    karma = require('karma').server,
-    del = require('del');
-
-// Check for --production flag
-var isProduction = !!(argv.production);
-
-// Check for --disable-es6 flag
-// enables babelify transforms and runtime, defaults to true
-var useES6 = !(argv.disableEs6);
+var g = require('gulp-load-plugins')();
+var gulp = require('gulp');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+var runSequence = require('run-sequence');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var karma = require('karma').server;
+var del = require('del');
 
 var babelifyConfig = {
   optional: ["runtime"],
@@ -30,37 +17,17 @@ var babelifyConfig = {
 };
 
 var paths = {
-  templates: [
-   './src/**/*.html'
-   // '!./src/index.html'
-  ],
   app: './src',
-  scripts: './src/js/**/*.js',
   scriptsEntry: './src/js/app.js',
   html: [
     './src/index.html'
   ],
-  dist: './dist',
   temp: './.tmp',
-  target: isProduction ? './dist' : './.tmp',
-  statics: [
-    './src/icons/**',
-    './src/fonts/**'
-  ],
-  images: [
-    './src/images/**'
-  ],
   sass: './src/scss/**/*.scss',
   sassEntry: './src/scss/main.scss'
 };
 
 gulp.task('default', ['watch']);
-
-gulp.task('dist', function (done) {
-  isProduction = true;
-  paths.target = paths.dist;
-  runSequence('clean', ['statics', 'scripts', 'sass'], 'html', done);
-});
 
 gulp.task('watch', ['serve'], function () {
   gulp.watch(paths.html, function (evt) {
@@ -86,38 +53,26 @@ gulp.task('test', function (done) {
 
 gulp.task('html', function () {
   return gulp.src(paths.html)
-    .pipe(gulp.dest(paths.target));
-});
-
-gulp.task('statics', function () {
-  return gulp.src(paths.statics, {
-    base: paths.app
-  }).pipe(gulp.dest(paths.dist));
+    .pipe(gulp.dest(paths.temp));
 });
 
 gulp.task('clean', function (done) {
-  del([paths.target, paths.temp], done);
+  del([paths.temp, paths.temp], done);
 });
 
 gulp.task('sass', function () {
   return gulp.src(paths.sassEntry)
-    .pipe(g.if(!isProduction, g.sourcemaps.init()))
-    .pipe(g.sass({
-      errLogToConsole: true
-    }))
-    .pipe(g.autoprefixer({
-      browsers: ['last 2 versions', 'ie 10']
-    }))
-    .pipe(g.if(!isProduction, g.sourcemaps.write()))
-    .pipe(g.if(isProduction, g.minifyCss()))
-    .pipe(g.if(isProduction, g.rev()))
-    .pipe(gulp.dest(paths.target + '/css'))
-    .pipe(g.if(!isProduction, g.connect.reload()));
+    .pipe(g.sourcemaps.init())
+    .pipe(g.sass({ errLogToConsole: true }))
+    .pipe(g.autoprefixer({ browsers: ['last 2 versions', 'ie 10'] }))
+    .pipe(g.sourcemaps.write())
+    .pipe(gulp.dest(paths.temp + '/css'))
+    .pipe(g.connect.reload());
 });
 
 gulp.task('serve', ['build'], function() {
   g.connect.server({
-    root: isProduction ? paths.dist : [paths.temp, paths.app],
+    root: [paths.temp, paths.app],
     port: 8080,
     livereload: true
   });
@@ -127,23 +82,14 @@ function browserifyShare() {
   var b = browserify(paths.scriptsEntry, {
     cache: {},
     packageCache: {},
-    debug: !isProduction
+    debug: true
   });
 
-  if (useES6) {
-    b.transform(
-      babelify.configure(babelifyConfig)
-    );
-  }
+  b.transform(babelify.configure(babelifyConfig));
 
-  var w;
-  if (!isProduction) {
-    w = watchify(b);
-
-    w.on('update', function() {
-      bundleShare(b);
-    });
-  }
+  watchify(b).on('update', function() {
+    bundleShare(b);
+  });
 
   b.on('log', g.util.log);
 
@@ -155,8 +101,6 @@ function bundleShare(b) {
     .pipe(g.plumber({errorHandler: g.notify.onError('<%= error.message %>')}))
     .pipe(source('app.js'))
     .pipe(buffer())
-    .pipe(g.if(isProduction, g.uglify()))
-    .pipe(g.if(isProduction, g.rev()))
-    .pipe(gulp.dest(paths.target + '/js'))
-    .pipe(g.if(!isProduction, g.connect.reload()));
+    .pipe(gulp.dest(paths.temp + '/js'))
+    .pipe(g.connect.reload());
 }
