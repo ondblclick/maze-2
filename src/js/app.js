83241675a53a2edaa61facec2178e649557343c3
Bohdan Hontarenko <tuoefil@gmail.com>
2015-07-20 09:53:03 +0300

    missing semicolons

--- src/js/app.js
+++ src/js/app.js
@@ -6,15 +6,15 @@ import $ from 'jquery';
 var maze;
 var width;
 var height;
-var checkPoint = 'start'
+var checkPoint = 'start';
 
 $(document).on('click', '#maze td', (e) => {
-  checkPoint = checkPoint === 'start' ? 'end' : 'start'
+  checkPoint = checkPoint === 'start' ? 'end' : 'start';
   $('#maze td').removeClass(checkPoint);
   $(e.currentTarget).addClass(checkPoint);
 
-  if($('#maze td.start').length && $('#maze td.end').length) {
-    solveMaze()
+  if ($('#maze td.start').length && $('#maze td.end').length) {
+    solveMaze();
   }
 });
 
@@ -23,15 +23,15 @@ var solveMaze = () => {
   var endCell = Cell.findBy(...$('.end').attr('id').split('-'));
   var solver = new Solver();
   var path = solver.findPath(startCell, endCell);
-  solver.render(path)
-}
+  solver.render(path);
+};
 
 window.generateAndDrawMaze = () => {
-  width = $('#width').val()
-  height = $('#height').val()
-  maze = new Maze(width, height)
-  maze.generateMaze()
-  maze.render()
-}
+  width = $('#width').val();
+  height = $('#height').val();
+  maze = new Maze(width, height);
+  maze.generateMaze();
+  maze.render();
+};
 
 generateAndDrawMaze();
